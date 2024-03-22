1996591d2edb6b951ffbf8d9880d165466da5bd8
callmeharis <theharis99@gmail.com>
2024-03-22 20:10:13 +0500

    TASK-MANAGER-03 || database setup and code formatting

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
-require('./db/connect')
 const express = require('express')
 const app = express()
 const tasks = require("./routes/tasks")
+const connectDB = require('./db/connect')
 
 app.use(express.json())
 
@@ -13,5 +13,12 @@ app.get("/", (req, res) => {
 app.use("/api/v1/tasks", tasks)
 
 const port = process.env.PORT
-
-app.listen(port, console.log(`Server is listening on port ${port}`))
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening on port ${port}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+start()
